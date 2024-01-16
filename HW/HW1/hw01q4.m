0d8768f8f20828eb152846ced4d27bc7fda3f438
omaraguilarr84 <omar.hawks8@gmail.com>
2024-01-16 18:04:07 -0500

    Started q4

---
+++ HW/HW1/hw01q4.m
@@ -0,0 +1,5 @@
+% Omar Aguilar + Nick Stiebler
+
+% Part 1
+printstuff(@nonsense, [4, 11, 15, 20])
+
---
+++ HW/HW1/nonsense.m
@@ -0,0 +1,9 @@
+% Omar Aguilar + Nick Stiebler
+
+function y = nonsense(x)
+    if mod(x,2) == 0
+        y = 0;
+    else
+        y = x + 1;
+    end
+end
