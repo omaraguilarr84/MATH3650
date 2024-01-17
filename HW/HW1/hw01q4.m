af420c5ca7f9d6f2d869e7766d9fdd8e3db949c0
Omar Aguilar <omar.hawks8@gmail.com>
2024-01-16 22:58:01 -0500

    Finished HW1, still needs to be reviewed

--- HW/HW1/hw01q4.m
+++ HW/HW1/hw01q4.m
@@ -1,5 +1,18 @@
 % Omar Aguilar + Nick Stiebler
 
 % Part 1
-printstuff(@nonsense, [4, 11, 15, 20])
+printstuff(@nonsense, [4, 11, 15, 20]);
 
+% Part 2
+x = 0:50;
+ns = length(x);
+for i = 0:50
+    ns(i+1) = nonsense(i);
+end
+
+figure;
+plot(x,ns,'o--');
+title('x vs. nonsense');
+legend('nonsense', 'Location', 'NorthWest');
+xlabel('x');
+ylabel('nonsense');
