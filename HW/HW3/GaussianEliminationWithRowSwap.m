ad2fa0ac4725f352fede20d55b79d391013820d8
omaraguilarr84 <omar.hawks8@gmail.com>
2024-02-05 12:17:15 -0500

    Finished row swapping but ge wrong

--- HW/HW3/GaussianEliminationWithRowSwap.m
+++ HW/HW3/GaussianEliminationWithRowSwap.m
@@ -4,15 +4,15 @@ function [x] = GaussianEliminationWithRowSwap(A,b)
 
 n = length(b);
 
-% Find largest absolute value in current column
-[~, linearIndex] = max(abs(A(:,1))); % Think I need to implement this within the loop but not sure
-[maxRow, ~] = ind2sub(size(A),linearIndex);
-    
-% Row swap
-A([1,maxRow],:) = A([maxRow,1],:); % How does row swap work past the first one
-b([1,maxRow],1) = b([maxRow,1],1);
-
 for k = 1:n-1
+    % Find largest absolute value in current column
+    [~, linearIndex] = max(abs(A(k:end,k))); % Think I need to implement this within the loop but not sure
+    [maxRow, ~] = ind2sub(size(A),linearIndex);
+    
+    % Row swap
+    A([k,maxRow],:) = A([maxRow,k],:); % How does row swap work past the first one
+    b([k,maxRow],1) = b([maxRow,k],1);
+    
     % Check if pivot is equal to zero
     if abs(A(k,k)) < 1e-15
         error('A has diagonal entries of zero')
