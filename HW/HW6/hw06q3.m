608a8fb007c465d2945796709b9f5a90658c5065
omaraguilarr84 <omar.hawks8@gmail.com>
2024-03-12 14:01:11 -0400

    finished hw6

--- HW/HW6/hw06q3.m
+++ HW/HW6/hw06q3.m
@@ -1,5 +1,9 @@
 % Omar Aguilar
+format long
 
-[root,numits] = newton(@myNDfun,@myNDfunprime,[0;0;1],10^-6)
+[root,numits] = newton(@myNDfun,@myNDfunprime,[0;0;0],10^-6);
+value = myNDfun(root);
 
-value = myNDfun(root)
+% Other root
+[root2,numits2] = newton(@myNDfun,@myNDfunprime,[1,1,8],10^-6);
+value = myNDfun(root2);
